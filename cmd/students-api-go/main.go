9405338712a6254181d910e9801898f06df6f0e2
Saurav Lal Karn <sauravkarn541@gmail.com>
2025-08-22 21:23:28 +0545

    Initial commit

---
+++ cmd/students-api-go/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Starting point of the project
+	fmt.Println("Starting the students api project")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/saurav-lal-karn/students-api-go
+
+go 1.24.2

23e0556f8413fd004f2eb75b27c70a5248dcbd22
Saurav Lal Karn <sauravkarn541@gmail.com>
2025-08-22 23:38:29 +0545

    graceful shutdown added

--- cmd/students-api-go/main.go
+++ cmd/students-api-go/main.go
@@ -1,8 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/saurav-lal-karn/students-api-go/internal/config"
+)
 
 func main() {
-	// Starting point of the project
-	fmt.Println("Starting the students api project")
+	// Load config
+	cfg := config.MustLoad()
+	// Setup loggers
+	// Setup database
+	// Setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+	// Setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server started:", slog.String("address", cfg.Addr))
+	// fmt.Printf("Server started: %s", cfg.HTTPServer.Addr)
+
+	// Run in goroutine for the graceful shutdown
+	// This is must in production
+	// otherwise ongoing request will be stopped immediately
+	// This is not preferred in production
+	done := make(chan os.Signal, 1)
+
+	// Get the interrupt signal from os as well as others as well
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	// Wait for signal to arrive in done channel
+	// Until then, it will not move forward
+	<-done
+
+	// Use structured log over traditional log
+	slog.Info("Shutting Down the Server")
+
+	// We can shutdown immediately
+	// But sometimes the system gets in infinite loop
+	// This will cause the system to never shut down
+	// So use timer to know if the server has stopped over certain time or not
+	// We need to do graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Above context waits for 5 seconds as mentioned in timeout
+	// If not, we will shutdown the server
+	// err := server.Shutdown(ctx)
+	// if err != nil {
+	// 	slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	// }
+
+	// Shorthand way to write above error condition
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successfully")
 }
